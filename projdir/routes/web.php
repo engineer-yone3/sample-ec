dc141a11dbd5b9ea40af4197257c06bfe917c180
engineer.yone3@gmail.com <engineer.yone3@gmail.com>
2023-02-20 18:32:01 +0900

    apache関連の設定を変更(Laravelでルーティングできなかったので)

--- projdir/routes/web.php
+++ projdir/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('admin')->group(function () {
+    Route::get('/', function () {
+        return view('admin.auth.login');
+    });
+    Route::post('login', [AuthController::class, 'adminLogin']);
 });

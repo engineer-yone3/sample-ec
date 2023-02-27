0c2337bda97a248deb6477e871ad2684d6ee4fc8
engineer.yone3@gmail.com <engineer.yone3@gmail.com>
2023-02-27 17:29:38 +0900

    Home画面でハンバーガーメニューを出そうと粘っているところ

--- projdir/resources/views/admin/layouts/admin.blade.php
+++ projdir/resources/views/admin/layouts/admin.blade.php
@@ -6,6 +6,8 @@
         content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}" >
   <title>{{ $htmlTitle }}</title>
   @yield('js-block')
   @yield('css-block')
--- projdir/resources/views/admin/layouts/nav.blade.php
+++ projdir/resources/views/admin/layouts/nav.blade.php
@@ -1,8 +1,19 @@
-<section>
-  <a href="{{ url(route('admin.home')) }}">{{ config('app.name') }}</a>
-</section>
-<nav>
-  <ul>
-    <li><a href="{{ url(route('admin.home')) }}">HOME</a></li>
-  </ul>
-</nav>
+{{--<div class="hamburger">--}}
+{{--  <section>--}}
+{{--    <a href="{{ url(route('admin.home')) }}">{{ config('app.name') }}</a>--}}
+{{--  </section>--}}
+{{--  <nav>--}}
+{{--    <ul>--}}
+{{--      <li><a href="{{ url(route('admin.home')) }}">HOME</a></li>--}}
+{{--    </ul>--}}
+{{--    <ul>--}}
+{{--      <li><a href="{{ url(route('admin.home')) }}">ユーザー管理</a></li>--}}
+{{--    </ul>--}}
+{{--    <ul>--}}
+{{--      <li><a href="{{ url(route('admin.home')) }}">商品管理</a></li>--}}
+{{--    </ul>--}}
+{{--  </nav>--}}
+{{--</div>--}}
+
+
+
